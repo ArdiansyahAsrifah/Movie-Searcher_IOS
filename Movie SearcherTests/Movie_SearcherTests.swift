f633494c9437f921ae8d6408dc6deddabaf53406
ArdiansyahAsrifah <ancakiky@gmail.com>
2025-03-01 04:33:03 +0700

    Initial Commit

---
+++ Movie SearcherTests/Movie_SearcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_SearcherTests.swift
+//  Movie SearcherTests
+//
+//  Created by Muhammad Ardiansyah Asrifah on 01/03/25.
+//
+
+import Testing
+@testable import Movie_Searcher
+
+struct Movie_SearcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
